a94592b0491bbe9df21649db12e77bf230defb48
Chauqni Sun <chuanqi.sun.dev@gmail.com>
2024-12-23 16:15:57 -0800

    add vision pipeline

--- src/storybox.ts
+++ src/storybox.ts
@@ -1,5 +1,16 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { debounceTime, filter, fromEvent, map, Observable, switchMap, tap } from "rxjs";
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  mergeMap,
+  Observable,
+  scan,
+  tap,
+} from "rxjs";
 import { AIBar } from "./lib/ai-bar/lib/ai-bar";
 import { CameraNode } from "./lib/ai-bar/lib/elements/camera-node";
 import type { LlmNode } from "./lib/ai-bar/lib/elements/llm-node";
@@ -78,62 +89,63 @@ const globalClick$ = fromEvent(document, "click").pipe(
         break;
       }
       case "show": {
-        const dataURL2Base64 = (dataURL: string) => {
-          const mimeType = dataURL.split(",")[0].split(":")[1].split(";")[0];
-          const data = dataURL.split(",")[1];
-          return {
-            data,
-            mimeType,
-          };
-        };
-
-        const openai = $<LlmNode>("llm-node")!.getClient("aoai");
-        openai.chat.completions
-          .create({
-            model: "gpt-4o-mini",
-            messages: [
-              system`Describe the image in one short sentence`,
-              {
-                role: "user",
-                content: [
-                  {
-                    type: "image_url",
-                    image_url: {
-                      url: cameraNode.capture(),
-                    },
-                  },
-                ],
-              },
-            ],
-          })
-          .then((response) => {
-            console.log("openai", response.choices[0].message.content);
-          });
-
-        chatSession
-          .sendMessage([
-            "Describe the image in one short sentence",
+        const frame = cameraNode.capture();
+        debugCapture.src = frame;
+        const llm = $<LlmNode>("llm-node")!;
+        const aoai = llm.getClient("openai");
+        const abortController = new AbortController();
+        const response = aoai.chat.completions
+          .create(
             {
-              inlineData: dataURL2Base64(cameraNode.capture()),
+              messages: [
+                system`Describe the objects on the desk and their relationships in one brief sentence.`,
+                {
+                  role: "user",
+                  content: [
+                    {
+                      type: "image_url",
+                      image_url: {
+                        url: frame,
+                      },
+                    },
+                  ],
+                },
+              ],
+              model: "gpt-4o-mini",
             },
-          ])
-          .then((result) => {
-            console.log("gemini", result.response.text());
-          });
+            {
+              signal: abortController.signal,
+            },
+          )
+          .then((response) => {
+            const description = response.choices[0].message.content;
+            console.log("latest summary", description);
+            debugCaption.textContent = description;
+          })
+          .catch();
+
+        break;
       }
     }
   }),
 );
 
+const pendingDescriptionCount$ = new BehaviorSubject(0);
+
 const vision$ = fromEvent(cameraNode, "framechange").pipe(
   debounceTime(1000),
-  switchMap(() => {
-    return new Observable((subscriber) => {
+  mergeMap(() => {
+    return new Observable<{
+      startedAt: number;
+      description: string;
+    }>((subscriber) => {
+      const startedAt = Date.now();
       const frame = cameraNode.capture();
       debugCapture.src = frame;
       const llm = $<LlmNode>("llm-node")!;
       const aoai = llm.getClient("openai");
       const abortController = new AbortController();
+      pendingDescriptionCount$.next(pendingDescriptionCount$.value + 1);
       const response = aoai.chat.completions
         .create(
           {
@@ -158,19 +170,59 @@ const vision$ = fromEvent(cameraNode, "framechange").pipe(
           },
         )
         .then((response) => {
-          const description = response.choices[0].message.content;
+          const description = response.choices[0].message.content ?? "";
           console.log("latest summary", description);
-          subscriber.next(description);
-          debugCaption.textContent = description;
+
+          subscriber.next({
+            startedAt,
+            description,
+          });
         })
-        .catch();
+        .catch()
+        .finally(() => {
+          pendingDescriptionCount$.next(pendingDescriptionCount$.value - 1);
+          subscriber.complete();
+        });
 
       return () => {
         abortController.abort();
       };
     });
   }),
+  scan(
+    (acc, curr) => {
+      const { startedAt, description } = curr;
+      if (startedAt > acc.timestamp) {
+        return {
+          timestamp: startedAt,
+          description,
+        };
+      } else {
+        return acc;
+      }
+    },
+    {
+      timestamp: 0,
+      description: "",
+    },
+  ),
+  distinctUntilChanged((prev, curr) => prev.description === curr.description),
+  tap((state) => {
+    debugCaption.textContent = state.description;
+  }),
+);
+
+const renderCaptionStatus$ = pendingDescriptionCount$.pipe(
+  tap((count) => {
+    const captionStatus = $<HTMLElement>("#caption-status")!;
+    if (!count) {
+      captionStatus.textContent = "Ready";
+    } else {
+      captionStatus.textContent = `Pending: ${count}`;
+    }
+  }),
 );
 
 globalClick$.subscribe();
 vision$.subscribe();
+renderCaptionStatus$.subscribe();
