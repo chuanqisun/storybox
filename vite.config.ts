19f3df1ba31e9cbb16d226bd03570177af963bba
chuanqisun <chuanqi.sun.dev@gmail.com>
2024-12-29 20:17:30 -0800

    udpate build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/storybox", // align with the GitHub Pages repository nameq
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        prototype: resolve(__dirname, "prototype.html"),
+      },
+    },
+  },
 });
